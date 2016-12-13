dcb8fb6f3b8fa0fae1743e8e5417466b8d187f49
Ketan Mirg <ketan.mirg@DEL1-DHP-27538.synapse.com>
2016-12-13 17:19:33 +0530

    maven setup done

---
+++ SMA/src/main/java/com/sma/socialapp/App.java
@@ -0,0 +1,13 @@
+package com.sma.socialapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
